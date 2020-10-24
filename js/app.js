2b70aa470b8b7ef2eff0400d111f83dc60d954c8
root <root@Vektur.localdomain>
2020-10-23 17:39:24 -0700

    null

--- js/app.js
+++ js/app.js
@@ -1,20 +1,9 @@
 'use strict';
 
-// Stores the min/max hourly customers, and the average cookies per customer, in object properties.
-
-//Uses a method of that object to generate a random number of customers per hour. Objects/Math/random
-
-// Calculate and store the simulated amounts of cookies purchased for each hour at each location using average cookies purchased and the random number of customers generated
-
-// Store the results for each location in a separate array… perhaps as a property of the object representing that location
-
-// Display the values of each array as unordered lists in the browser
-
-// Calculating the sum of these hourly totals; your output for each location should look like this:
-
 var hours = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm'];
 
 var salesTable = document.getElementById('salesTable');
+
 
 var tr = document.createElement('tr');
 salesTable.appendChild(tr);
@@ -23,18 +12,15 @@ th.textContent = '';
 tr.appendChild(th);
 for (var i = 0; i < hours.length; i++) {
   th = document.createElement('th');
-  th.textContent = (hours[i]);
+  th.textContent = hours[i];
   tr.appendChild(th);
 }
+
 
 th = document.createElement('th');
 th.textContent = 'Total';
 tr.appendChild(th);
 
-
-
-
-console.log(salesTable);
 
 function Sales(storeName, minCust, maxCust, avgCookies) {
   this.storeName = storeName;
@@ -53,6 +39,10 @@ function Sales(storeName, minCust, maxCust, avgCookies) {
       this.dailySales += hrlySales;
     }
   };
+  this.totals = function () {
+    
+  };
+
   this.render = function () {
     var tr = document.createElement('tr');
     var td = document.createElement('td');
@@ -66,19 +56,33 @@ function Sales(storeName, minCust, maxCust, avgCookies) {
     }
 
     td = document.createElement('td');
-    td.textContent = this.dailySales ;
+    td.textContent = this.dailySales;
     tr.appendChild(td);
   };
+
 }
 
+
+
 var tfoot = document.createElement('tfoot');
-var td = document.createElement(td);
-td.textContent = 'Total';
-tfoot.appendChild(td);
-// var td = document.createElement(td);
-// td.textContent = 0;
-// tfoot.appendChild(td);
 salesTable.appendChild(tfoot);
+var thead = document.createElement(thead);
+thead.textContent = 'Total';
+tfoot.appendChild(thead);
+var td = document.createElement(td);
+
+
+// var totals = function () {
+//   for (var i = 0; i < seattleStore.cookiesPerHr.length; i++) {
+//     for (var j = 0; j < seattleStore.cookiesPerHr[i]; j++) {
+//       var total = 0;
+//       total = (total += seattleStore.cookiesPerHr[j][i]);
+//     }
+//   }
+// };
+
+
+
 
 var seattleStore = new Sales('Seattle', 23, 65, 6.3);
 var toykoStore = new Sales('Tokyo', 3, 24, 1.2);
@@ -86,23 +90,19 @@ var dubaiStore = new Sales('Duabi', 11, 38, 3.7);
 var parisStore = new Sales('Paris', 20, 38, 2.3);
 var limaStore = new Sales('Lima', 2, 16, 4.6);
 
-seattleStore.getRdmCust();
+
 seattleStore.getCookiesPerhour();
 seattleStore.render();
 
-toykoStore.getRdmCust();
 toykoStore.getCookiesPerhour();
 toykoStore.render();
 
-dubaiStore.getRdmCust();
 dubaiStore.getCookiesPerhour();
 dubaiStore.render();
 
-parisStore.getRdmCust();
 parisStore.getCookiesPerhour();
 parisStore.render();
 
-limaStore.getRdmCust();
 limaStore.getCookiesPerhour();
 limaStore.render();
 
